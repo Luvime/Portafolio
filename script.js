9d06218e71573372c38c11df60c079ed5c1c4346
Luvime <luiis.mediina9456@gmail.com>
2023-11-26 15:43:15 -0300

    primer commit script

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+function responsiveMenu() {
+    var x = document.getElementById("nav")
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
