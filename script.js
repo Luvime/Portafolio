46737a3af1b22c332cb4dacf366fee1a8268412e
Luvime <luiis.mediina9456@gmail.com>
2023-11-26 16:30:55 -0300

    cambios css y script

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+//funcion que me aplica el estilo a la opcion seleccionada en el menu quita la previamente seleccionado
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
 
-
-
+    //hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+//funcion que muestra el menu responsive
 function responsiveMenu() {
-    var x = document.getElementById("nav")
+    var x = document.getElementById("nav");
     if (x.className === "") {
         x.className = "responsive";
     } else {
